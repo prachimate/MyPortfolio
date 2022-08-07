fc2bfe32b768f410f87d8e786e23439fe2b85253
prachimate <72977963+prachimate@users.noreply.github.com>
2022-08-07 20:55:59 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+const menu_btn = document.querySelector(".menu-btn");
+const hamburger = document.querySelector(".menu-btn__burger");
+let showMenu = false;
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav__item");
+menu_btn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    hamburger.classList.add("open");
+    nav.classList.add("open");
+    menuNav.classList.add("open");
+    navItems.forEach((item) => item.classList.add("open"));
+    showMenu = true;
+  } else {
+    hamburger.classList.remove("open");
+    nav.classList.remove("open");
+    menuNav.classList.remove("open");
+    navItems.forEach((item) => item.classList.remove("open"));
+    showMenu = false;
+  }
+}
